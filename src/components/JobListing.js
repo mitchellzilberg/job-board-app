7c69210a1f16e0606e03ce48c9eb6fd5138dd647
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2021-03-06 14:13:57 -0500

    try to fix image path

--- src/components/JobListing.js
+++ src/components/JobListing.js
@@ -20,7 +20,7 @@ const JobListing = ({job: {
     featured && 'border-l-4 border-indigo-300 border-solid'
   } lg:flex-row lg:my-4`}>
     <div>
-      <img className="-mt-12 mb-4 w-20 h-20 lg:mt-0 lg:h-24 lg:w-24 lg:my-0"  src={logo} alt={company} />
+      <img className="-mt-12 mb-4 w-20 h-20 lg:mt-0 lg:h-24 lg:w-24 lg:my-0"  src="https://mitchellzilberg.github.io/job-board-app/{logo}" alt={company} />
     </div>
     <div className="flex flex-col justify-between ml-3">
       <h3 className="font-bold text-md text-indigo-400">  {company} 
